3defdc01799be0f7596c6750ad29c0b568c72e3c
Joe Ryan <joe@Joes-iMac.local>
2020-05-10 18:22:27 +0100

    set up amplify and react router

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export default {
+    apiGateway: {
+      REGION: "eu-west-1",
+      URL: "https://97nvmby780.execute-api.eu-west-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "eu-west-1",
+      USER_POOL_ID: "eu-west-1_oBLFRrUQx",
+      APP_CLIENT_ID: "730ckmo3verp13r6u544vbfo42",
+      IDENTITY_POOL_ID: "eu-west-1:f245f314-9fb4-4d3e-a94b-47f8cbf4a10a"
+    }
+  };
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Pub Quiz</h1>
+        <p>Hello User</p>
+      </div>
+    </div>
+  );
+}
